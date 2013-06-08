36eabdaa96e1e47162bb7a406cb8fc69e1141d87
Sam Stokes <sam@rapportive.com>
2013-06-07 19:39:41 -0700

    Possibly unsuccessful attempt at standalone slidy

--- OTL/src/Main.hs
+++ OTL/src/Main.hs
@@ -22,6 +22,7 @@ import Text.OTL.Pandoc
 
 import Text.ParserCombinators.Parsec (ParseError)
 import System.Exit (exitFailure)
+import Text.Pandoc (WriterOptions(..))
 import qualified Text.Pandoc as Pandoc
 import System.Environment (getArgs)
 import Data.Maybe (fromMaybe)
@@ -36,11 +37,16 @@ handleParse :: Either ParseError Outline -> IO ()
 handleParse (Left err) = print err >> exitFailure
 handleParse (Right outline) = do
     args <- getArgs
-    let outputFormat = case args of
-                        [format] -> format
-                        [] -> "html"
-                        _ -> error $ "bad args " ++ show args
+    let (outputFormat, opts) = case args of
+            format : rest -> (format, rest)
+            [] -> error $ "bad args " ++ show args
     let writer = fromMaybe (error $ "can't write " ++ outputFormat) $ lookup outputFormat Pandoc.writers
     options <- defaultWriterOptions outputFormat
+    let options' = case opts of
+                [] -> options
+                ["--self-contained"] ->
+                    options { writerVariables = ("slidy-url", "slidy") : writerVariables options,
+                              writerStandalone = True }
+                _ -> error $ "bad options " ++ show opts
     let pandoc = toPandoc outline
-    putStrLn $ writer options $ pandoc
+    putStrLn $ writer options' $ pandoc
